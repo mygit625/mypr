48ec68d4c31624cc3b95529344cc421c8830d2e1
Rashad Farzalizada <farzalizada.rashad.ilhamin.2023@unec.edu.az>
2025-09-07 07:05:24 +0000

    here is the code for you:
    <!-- Hotjar Tracking Code for Toolsinn -->
    <s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,19 @@ export default function RootLayout({
             gtag('config', 'G-3FCG8RPTMG');
           `}
         </Script>
+        {/* Hotjar Tracking Code for Toolsinn */}
+        <Script id="hotjar-tracking">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6513077,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} ${dancingScript.variable} ${pacifico.variable} ${caveat.variable} antialiased flex flex-col min-h-screen bg-background`}
